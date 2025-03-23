40be94f3f3009155d947e94f7766ba320a8c6952
BugKiller099 <myashifjamal@gmail.com>
2025-03-23 15:26:35 +0530

    build basic authentication so that user can not see profile before they login

--- components/Body.jsx
+++ components/Body.jsx
@@ -1,17 +1,45 @@
-
-
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import { BASE_URL } from "../src/utils/constants";
+import axios from "axios";
+import { addUser } from "../src/utils/userSlice";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
 
-const Body =() =>{
-    return (
-        <div>
-            <NavBar />
-            <Outlet />
-            <Footer />
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.response?.status === 401) {
+        navigate("/login");
+      }
+      console.error(err);
+    }
+  };
 
-        </div>
-    );
+  useEffect(() => {
+    if(!userData)
+    {
+        fetchUser();
+    }
+    
+  }, []);
+
+  return (
+    <div>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
+
 export default Body;
