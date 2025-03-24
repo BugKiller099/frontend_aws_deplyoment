99e10892fcfe41a73123e22616c4e1e74c3ecee4
BugKiller099 <myashifjamal@gmail.com>
2025-03-24 13:42:35 +0530

    added profile edit page and created card to update profile EditProfile.jsx

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import EditProfile from './EditProfile'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+
+  const user = useSelector((store) => store.user);
   return (
+   user ? (
     <div>
-      Profile
+    <EditProfile user={user}/>
     </div>
+   ) : null
   )
 }
+   
+
 
 export default Profile
 
