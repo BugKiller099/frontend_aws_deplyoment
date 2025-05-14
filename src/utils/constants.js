b3fbc721b8e20ca6dd4a2c5d06270f40cb775575
BugKiller099 <myashifjamal@gmail.com>
2025-05-14 16:27:09 +0530

    added base url to socket.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,4 @@
 
 
 export const BASE_URL ="/api"
+export const VITE_SOCKET_URL="http://3.110.187.101"
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,14 @@
 import io from "socket.io-client";
-import { BASE_URL } from "./constants";
+import { VITE_SOCKET_URL } from "./constants";
 
-export const createSocketConnection =()=>{
-    return io(BASE_URL);
-};
+// export const createSocketConnection =()=>{
+//     return io(BASE_URL);
+// };
 
+export const createSocketConnection = () => {
+    return io(VITE_SOCKET_URL, {
+      transports: ["websocket"],
+      withCredentials: true,
+    });
+  };
+  
