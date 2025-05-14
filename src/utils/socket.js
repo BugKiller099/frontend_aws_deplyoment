f7cb5285b33f34d38f7f9d909002c18f3cd79004
BugKiller099 <myashifjamal@gmail.com>
2025-05-15 02:10:36 +0530

    Fixed commit

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -7,8 +7,20 @@ import { VITE_SOCKET_URL } from "./constants";
 
 export const createSocketConnection = () => {
     return io(VITE_SOCKET_URL, {
+        
       transports: ["websocket"],
       withCredentials: true,
     });
   };
+  
+
+//   export const createSocketConnection = () => {
+//     return io(VITE_SOCKET_URL, {
+//       transports: ['websocket'],
+//       withCredentials: true,
+//       path: '/socket.io/',       // optional unless server uses custom path
+//       reconnection: true,
+//       reconnectionAttempts: 5,
+//     });
+//   };
   
